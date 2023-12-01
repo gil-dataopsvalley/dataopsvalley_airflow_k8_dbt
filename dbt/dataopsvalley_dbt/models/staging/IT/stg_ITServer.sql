0a91491ce974a3298cb55219021a9be29af7adde
Gil Kal <gilc86@gmail.com>
2023-12-01 15:43:37 +0200

    config poject and test first model

--- dbt/dataopsvalley_dbt/models/staging/IT/stg_ITServer.sql
+++ dbt/dataopsvalley_dbt/models/staging/IT/stg_ITServer.sql
@@ -1,7 +1,7 @@
 
 select
+    Office,
     Cluster,
-    b.Yuval_Customer as Office,
     VMName,
     VMHostName,
     cast( RAM as NUMERIC) as RAM,
@@ -12,6 +12,4 @@ select
     {{ isCurrentDay() }} as LastUpdate
 
 from {{ source('src_IT', 'MRR_Servernew') }} as a
-inner join {{ source('src_IT', 'MRR_MappingITServerCustomerToYuval') }} as b
-on a.Office = b.IT_Server
 
